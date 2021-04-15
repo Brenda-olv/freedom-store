2ce4bdc3ac1e85aca71ea5dcc819f629b36b1522
brenda barbosa <brenda.barbosa@finnet.com.br>
2021-04-15 17:15:18 -0300

    Estrutura components NavBar + Footer do projeto

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <nav-bar></nav-bar>
+    <footer-nav></footer-nav>
+  </div>
+</template>
+
+<script>
+import FooterNav from "@/common/components/FooterNav";
+import NavBar from "@/common/components/NavBar";
+
+export default {
+  name: "App",
+  components: { NavBar, FooterNav },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
